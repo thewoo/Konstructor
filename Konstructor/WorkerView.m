710aac5072be1929c4642abb21972040fefd1a44
Thecafremo <thewoo_pardo@hotmail.com>
2013-04-28 18:40:39 +0200

    Algunas cambios que no tengo muy claro de qué son.

---
+++ Konstructor/WorkerView.m
@@ -0,0 +1,28 @@
+//
+//  WorkerView.m
+//  Konstructor
+//
+//  Created by Thecafremo on 20/04/13.
+//  Copyright (c) 2013 TreeNovum. All rights reserved.
+//
+
+#import "WorkerView.h"
+
+@implementation WorkerView
+
+
+#pragma mark View's.
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        
+    }
+    
+    
+    return self;
+}
+
+@end
